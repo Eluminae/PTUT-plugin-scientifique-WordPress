93e2192aeb7704bc883a1007c55f44935f3f091d
Eluminae <eddiebarraco@live.fr>
2016-01-16 12:02:03 +0100

    ajut du dossier ou sera situé le plugin

---
+++ plugins/ScientificArticle/ScientificArticle.php
@@ -0,0 +1,12 @@
+< ?php
+/**
+ * Plugin Name: ScientificArticle
+ * Plugin URI: URI du plugin (si vous le mettez a disposition sur un site)
+ * Description: Description du plugin et de son contenu
+ * Version: Version du plugin (si vous le faites évoluer avec un système de version)
+ * Author: IUT Informatique Lyon 1
+ * Author URI: l'URI de votre site
+ * License: La ou les license(s) relative(s) à votre plugin
+ */
+ 
+ 
