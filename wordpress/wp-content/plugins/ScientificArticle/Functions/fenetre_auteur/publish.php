3c23e61503aff70abbd4a3d33c0af5c1a4477961
Eluminae <eddiebarraco@live.fr>
2016-01-17 15:48:38 +0100

    correction des valeures dans la bd

--- wordpress/wp-content/plugins/ScientificArticle/Functions/fenetre_auteur/publish.php
+++ wordpress/wp-content/plugins/ScientificArticle/Functions/fenetre_auteur/publish.php
@@ -11,18 +11,18 @@
 function ScientificArticle_auteur_save($post_id){
     // Checks for input and sanitizes/saves if needed
     if( isset( $_POST[ 'auteur_nom' ] ) ) {
-        update_post_meta( $post_id, '_auteur_nom', sanitize_text_field( $_POST[ 'auteur_nom' ] ) );
+        update_post_meta( $post_id, '_ScientificArticle_auteur_nom', sanitize_text_field( $_POST[ 'auteur_nom' ] ) );
     }
     if( isset( $_POST[ 'auteur_prenom' ] ) ) {
-        update_post_meta( $post_id, '_auteur_prenom', sanitize_text_field( $_POST[ 'auteur_prenom' ] ) );
+        update_post_meta( $post_id, '_ScientificArticle_auteur_prenom', sanitize_text_field( $_POST[ 'auteur_prenom' ] ) );
     }
     if( isset( $_POST[ 'auteur_profession' ] ) ) {
-        update_post_meta( $post_id, '_auteur_profession', sanitize_text_field( $_POST[ 'auteur_profession' ] ) );
+        update_post_meta( $post_id, '_ScientificArticle_auteur_profession', sanitize_text_field( $_POST[ 'auteur_profession' ] ) );
     }
     if( isset( $_POST[ 'auteur_affiliation' ] ) ) {
-        update_post_meta( $post_id, '_auteur_affiliation', sanitize_text_field( $_POST[ 'auteur_affiliation' ] ) );
+        update_post_meta( $post_id, '_ScientificArticle_auteur_affiliation', sanitize_text_field( $_POST[ 'auteur_affiliation' ] ) );
     }
     if( isset( $_POST[ 'auteur_site' ] ) ) {
-        update_post_meta( $post_id, '_auteur_site', sanitize_text_field( $_POST[ 'auteur_site' ] ) );
+        update_post_meta( $post_id, '_ScientificArticle_auteur_site', sanitize_text_field( $_POST[ 'auteur_site' ] ) );
     }
 }
