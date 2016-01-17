d9a40b07ce97430813c20aca9ed8ff7ed4753cad
Therence <th.brune@lapsote.net>
2016-01-17 15:42:29 +0100

    sauvegarde auteur

--- wordpress/wp-content/plugins/ScientificArticle/Functions/fenetre_auteur/publish.php
+++ wordpress/wp-content/plugins/ScientificArticle/Functions/fenetre_auteur/publish.php
@@ -8,6 +8,21 @@
 
 
 // ici on vas devoir enregistrer l'auteur dans la future BD
-function ScientificArticle_auteur_save(){
-
+function ScientificArticle_auteur_save($post_id){
+    // Checks for input and sanitizes/saves if needed
+    if( isset( $_POST[ 'auteur_nom' ] ) ) {
+        update_post_meta( $post_id, '_auteur_nom', sanitize_text_field( $_POST[ 'auteur_nom' ] ) );
+    }
+    if( isset( $_POST[ 'auteur_prenom' ] ) ) {
+        update_post_meta( $post_id, '_auteur_prenom', sanitize_text_field( $_POST[ 'auteur_prenom' ] ) );
+    }
+    if( isset( $_POST[ 'auteur_profession' ] ) ) {
+        update_post_meta( $post_id, '_auteur_profession', sanitize_text_field( $_POST[ 'auteur_profession' ] ) );
+    }
+    if( isset( $_POST[ 'auteur_affiliation' ] ) ) {
+        update_post_meta( $post_id, '_auteur_affiliation', sanitize_text_field( $_POST[ 'auteur_affiliation' ] ) );
+    }
+    if( isset( $_POST[ 'auteur_site' ] ) ) {
+        update_post_meta( $post_id, '_auteur_site', sanitize_text_field( $_POST[ 'auteur_site' ] ) );
+    }
 }
