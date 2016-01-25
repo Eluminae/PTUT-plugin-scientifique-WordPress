4da18724aa843f1e7ab54b9c41d035b513b85c80
ThomasGille <totogille@gmail.com>
2016-01-25 15:03:44 +0100

    t

---
+++ wordpress/wp-content/plugins/ScientificArticle/Functions/fenetre_article/publish.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noke
+ * Date: 25/01/16
+ * Time: 10:46
+ */
+
+// ici on vas devoir enregistrer l'auteur dans la future BD
+function ScientificArticle_article_save($post_id)
+{
+
+    if (get_post_type($post_id) == "sa_article") {
+
+
+
+        /*
+        $file = fopen("test.txt", 'c+');
+        fwrite($file, print_r($liste_auteur));
+        */
+
+        if (isset($_POST['meta-checkbox'])) {
+
+            foreach($_POST['meta-checkbox'] as $row){
+                $liste_auteur[] = $row;
+            }
+
+            update_post_meta($post_id, '_ScientificArticle_article_auteurs', serialize($liste_auteur));
+        }
+    }
+
+}
+
