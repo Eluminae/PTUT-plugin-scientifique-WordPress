725cc2b6e69d3cb59d178ffbfe3017ddba98ad26
ThomasGille <totogille@gmail.com>
2016-01-26 17:40:17 +0100

    Affichage par miniature & préparation de la mise en page article seul

--- wordpress/wp-content/themes/twentysixteen/single-SA_article.php
+++ wordpress/wp-content/themes/twentysixteen/single-SA_article.php
@@ -25,26 +25,12 @@ get_header();
               comments_template();
               }
              */
+            
             echo '<br />////////////<br />';
-            ?>
-            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-                <h1>
-                    <?php
-                    echo the_title();
-                    ?>
-                    <br /></h1>
-                <?php
-                $attachments = get_children(array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order'));
-                if (!is_array($attachments))
-                    continue;
-                $count = count($attachments);
-                $first_attachment = array_shift($attachments);
-                echo wp_get_attachment_image($first_attachment->ID, array(200, 200)); //afiche l'image en 500x500
-                ?></a>'<?php
-                echo get_the_content();
+            
+            echo '<br />////////////<br />';
 
-                echo '<br />////////////<br />';
-
+            
                 if (is_singular('attachment')) {
                     // Parent post navigation.
                     the_post_navigation(array(
