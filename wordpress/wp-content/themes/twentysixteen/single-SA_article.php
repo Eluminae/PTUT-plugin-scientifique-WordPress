8f1acd3d9ce568150f0b131d31483f17b9669346
ThomasGille <totogille@gmail.com>
2016-01-26 17:28:31 +0100

    Affichage par miniature

--- wordpress/wp-content/themes/twentysixteen/single-SA_article.php
+++ wordpress/wp-content/themes/twentysixteen/single-SA_article.php
@@ -29,10 +29,10 @@ get_header();
             ?>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
                 <h1>
-                <?php
-                echo the_title();
-                ?>
-                <br /></h1>
+                    <?php
+                    echo the_title();
+                    ?>
+                    <br /></h1>
                 <?php
                 $attachments = get_children(array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order'));
                 if (!is_array($attachments))
@@ -41,35 +41,35 @@ get_header();
                 $first_attachment = array_shift($attachments);
                 echo wp_get_attachment_image($first_attachment->ID, array(200, 200)); //afiche l'image en 500x500
                 ?></a>'<?php
-            echo get_the_content();
-           
-            echo '<br />////////////<br />';
+                echo get_the_content();
 
-            if (is_singular('attachment')) {
-                // Parent post navigation.
-                the_post_navigation(array(
-                    'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen'),
-                ));
-            } elseif (is_singular('post')) {
-                // Previous/next post navigation.
-                the_post_navigation(array(
-                    'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next', 'twentysixteen') . '</span> ' .
-                    '<span class="screen-reader-text">' . __('Next post:', 'twentysixteen') . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                    'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'twentysixteen') . '</span> ' .
-                    '<span class="screen-reader-text">' . __('Previous post:', 'twentysixteen') . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                ));
-            }
-            echo 'Fin de l\'affichage d\'un seul article';
+                echo '<br />////////////<br />';
 
-        // End of the loop.
-        endwhile;
-        ?>
+                if (is_singular('attachment')) {
+                    // Parent post navigation.
+                    the_post_navigation(array(
+                        'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen'),
+                    ));
+                } elseif (is_singular('post')) {
+                    // Previous/next post navigation.
+                    the_post_navigation(array(
+                        'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next', 'twentysixteen') . '</span> ' .
+                        '<span class="screen-reader-text">' . __('Next post:', 'twentysixteen') . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                        'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'twentysixteen') . '</span> ' .
+                        '<span class="screen-reader-text">' . __('Previous post:', 'twentysixteen') . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                    ));
+                }
+                echo 'Fin de l\'affichage d\'un seul article';
+
+            // End of the loop.
+            endwhile;
+            ?>
 
     </main><!-- .site-main -->
 
-<?php get_sidebar('content-bottom'); ?>
+    <?php get_sidebar('content-bottom'); ?>
 
 </div><!-- .content-area -->
 
